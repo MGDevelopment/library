1e01125ec5c513aea186df8da61e70bd0f1de42b
Alecco <info@alejosanchez.com>
2011-10-14 13:29:04 -0300

    More testing

--- python/tests/config.py
+++ python/tests/config.py
@@ -3,9 +3,16 @@ from ecommerce.config import Config
 
 class TestSequenceFunctions(unittest.TestCase):
 
+    def load_config(self, paths):
+        '''Helper function loads a configuration'''
+        Config(paths)
+
     def setUp(self):
         pass
 
     def test_config_load(self):
         c = Config(paths=('./tests/files',))  # Just load configuration
-        print c
+        self.assertTrue(c.len() > 0, 'empty configuration')
+
+    def test_config_bad_load(self):
+        self.assertRaises(IOError, self.load_config, ('/nonexistent',))
