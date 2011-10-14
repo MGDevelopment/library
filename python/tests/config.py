72604e1feefcdc7131e3a2e700c546deaf908cf6
Alecco <info@alejosanchez.com>
2011-10-14 15:22:04 -0300

    More test work

--- python/tests/config.py
+++ python/tests/config.py
@@ -1,18 +1,59 @@
-from ecommerce.config import Config
-import unittest
+from ecommerce.config import Config, global_name, local_name
+from unittest         import TestCase
+from tempfile         import mkdtemp
+from shutil           import rmtree
+from os               import chmod, remove
+from os.path          import join as pjoin
 
-class TestSequenceFunctions(unittest.TestCase):
+#
+# Test configuration files
+#
+global_conf = '''
+---
+top_global:  &top_global
+  inner_one:   one
+  inner_two:   bad
+'''
+
+local_conf = '''
+top:
+  <<: *top_global
+  inner_two:   two
+  inner_three: three
+'''
+
+class TestSequenceFunctions(TestCase):
 
     def load_config(self, paths):
         '''Helper function loads a configuration'''
         Config(paths)
 
     def setUp(self):
-        pass
+        '''
+        Create a temporary directory with sample configuration files
+        <tmp_dir>/<global_name>
+        <tmp_dir>/<local_name>
+        '''
+        self.tmp_dir    = mkdtemp()
+        self.tmp_global = pjoin(self.tmp_dir, global_name)
+        self.tmp_local  = pjoin(self.tmp_dir, local_name)
+        open(self.tmp_global, 'w').write(global_conf)
+        open(self.tmp_local,  'w').write(local_conf)
+
+    def tearDown(self):
+        '''Remove the temporary directory'''
+        rmtree(self.tmp_dir)
 
     def test_config_load(self):
-        c = Config(paths=('./tests/files',))  # Just load configuration
+        '''Test loading configuration files'''
+        c = Config(paths=(self.tmp_dir,))
         self.assertTrue(c.len() > 0, 'empty configuration')
 
-    def test_config_bad_load(self):
-        self.assertRaises(IOError, self.load_config, ('/nonexistent',))
+    def test_config_bad_perms(self):
+        chmod(self.tmp_global, 0111) # Change to execute only
+        self.assertRaises(IOError, self.load_config, (self.tmp_dir,))
+
+    def test_config_missing(self):
+        remove(self.tmp_global)
+        self.assertRaises(IOError, self.load_config, (self.tmp_dir,))
+
