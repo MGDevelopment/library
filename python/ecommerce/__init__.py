74cad1d954068a0cdf9755af22e7d3338aeeb96d
Alecco <info@alejosanchez.com>
2011-10-13 22:02:50 -0300

    Fix list

--- python/ecommerce/__init__.py
+++ python/ecommerce/__init__.py
@@ -5,7 +5,8 @@ by Alejo Sanchez, Jose Luis Campanello and Mariano Goldsman
 # Initialize module
 # Find this user/host configuration
 #   Try current directory or standard locations
-dirs = [ "./config", "/etc/ecommerce", 
+dirs = [ "./config", "/etc/ecommerce" ]
+
 # Exported names
 __all__ = [ "test" ]
 
