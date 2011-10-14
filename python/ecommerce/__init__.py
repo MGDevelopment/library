604db3dd560ec2310ff74b894f3a5b5736759fd3
Alecco <info@alejosanchez.com>
2011-10-14 03:41:08 -0300

    Add files to config unit test

--- python/ecommerce/__init__.py
+++ python/ecommerce/__init__.py
@@ -2,13 +2,5 @@
 by Alejo Sanchez, Jose Luis Campanello and Mariano Goldsman
 """
 
-# Initialize module
-# Find this user/host configuration
-#   Try current directory or standard locations
-dirs = [ "./config", "/etc/ecommerce" ]
-
 # Exported names
-__all__ = [ "test" ]
-
-if __name__ == "__main__":
-    print "Exports: ", __all__
+__all__ = [ "config", 'db', 'content' ]
