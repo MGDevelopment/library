0c518cb38f203d751ba181a9b756363657519bb8
Alecco <info@alejosanchez.com>
2011-10-20 13:47:12 -0300

    Fixes for uploader

--- python/ecommerce/upload/__init__.py
+++ python/ecommerce/upload/__init__.py
@@ -36,7 +36,6 @@ class S3Uploader(object):
     '''S3 content uploader
     
        params:
-           conn:                  boto S3 connection object
            bucket_name:           name of the bucket to upload
            directory:             destination directory (key prefix)
            gzip:                  should uploader gzip content (True)
@@ -44,13 +43,15 @@ class S3Uploader(object):
            AWS_ACCESS_KEY_ID:     AWS key id
            AWS_SECRET_ACCESS_KEY: AWS secret key
     '''
-    def __init__(self, conn, bucket_name, folder = '', gzip = True,
-           cache = 'public,max-age=3600',
+    def __init__(self, bucket_name, directory = None, gzip = True,
+           cache_type = 'public,max-age=3600',
            AWS_ACCESS_KEY_ID = None, AWS_SECRET_ACCESS_KEY = None):
-        self._conn       = conn
-        self._bucket     = conn.get_bucket_name(bucket_name)
+        self._conn       = connect_s3(AWS_ACCESS_KEY_ID,
+                                      AWS_SECRET_ACCESS_KEY)
+        self._bucket     = self._conn.get_bucket(bucket_name)
         self._gzip       = gzip
-        self._cache_type  = cache_type
+        self._cache_type = cache_type
+        self._directory  = directory
 
     def send(self, name, src, type):
         '''Upload an object to S3
@@ -61,7 +62,11 @@ class S3Uploader(object):
 
         # Create key
         key     = Key(self._bucket)
-        key.key = name
+        if self._directory:
+            key.key = '/'.join((self._directory, name))
+        else:
+            key.key = name
+
         # Headers
         key.set_metadata('Content-Type', type)
         if self._cache_type:
@@ -69,9 +74,9 @@ class S3Uploader(object):
         # Note: S3 already sets Etag
 
         fbuf = StringIO()  # Temporary in-memory virtual file
-        if self.gzip and type in content_gzippable:
+        if self._gzip and type in content_gzippable:
             # Compressed
-            zf = GzipFile(fname, 'wb', 9, fbuf)
+            zf = GzipFile(name, 'wb', 9, fbuf)
             zf.write(src)
             zf.close()
             key.set_metadata('Content-Encoding', 'gzip')
@@ -82,4 +87,6 @@ class S3Uploader(object):
         # Upload
         key.set_contents_from_file(fbuf, policy = 'public-read',
                 reduced_redundancy = True)
+
+        print key.generate_url(10)
 
