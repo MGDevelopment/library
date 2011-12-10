58916423241735778d91ee96c9c4ee59d9b84cd8
Jose Luis Campanello <jose.luis.campanello@gmail.com>
2011-12-10 18:37:48 -0300

    Added encryption to keychain. Created also a command to encrypt passwords (keygen)

--- python/ecommerce/config/keygen.py
+++ python/ecommerce/config/keygen.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-"""Config file keychain
+"""Key generation program
 
 This file contains a class that implements password management. The
 class is created inside a Config object and handles access to
 passwords.
 """
+
+import sys
 
 import random
 import hashlib
@@ -184,9 +186,8 @@ class Keychain(object):
         if "data" not in keyData:
             raise KeyError("Keychain Key [%s] has no data" % keyName)
 
-        # get the encripted value and convert to binary (input is hexa string)
+        # get the encripted value
         ciphertext = self._keychain[keyName]["data"].get(keyValue)
-        ciphertext = "".join([ chr(int(ciphertext[i:i+2], 16)) for i in range(0, len(ciphertext), 2) ])
 
         # get the 3des key and iv
         mKey = self._mkSeedDigest[0:24]
@@ -197,3 +198,215 @@ class Keychain(object):
         value = cipher.decrypt(ciphertext)
 
         return value
+
+###################################################
+
+_mkSeed         = None
+_mkSeedDigest   = None
+
+def _masterKey(master):
+
+    global _mkSeed
+    global _mkSeedDigest
+
+    # initialize the random seed
+    random.seed(master)
+
+    # generate 4kb random data to calculate the master key
+    _mkSeed        = "".join( [ "%02.2x" % random.randint(0, 255)
+                                for i in range(4096) ] )
+    _mkSeedDigest  = hashlib.sha512(_mkSeed).digest()
+
+
+###################################################
+
+def fcn_3DES_CBC(isCrypt, master, text):
+
+    # process the master key
+    _masterKey(master)
+
+    # prepare the input text
+    inputText = text
+    if not isCrypt:
+        # is decrypt and the input is hexa string
+        inputText = "".join([ chr(int(text[i:i+2], 16)) for i in range(0, len(text), 2) ])
+
+    # get the 3des key and iv
+    mKey = _mkSeedDigest[0:24]
+    mIV  = _mkSeedDigest[-8:]
+
+    # create the triple-des object and decrypt
+    cipher = pyDes.triple_des(mKey, pyDes.CBC, mIV, pad=None, padmode=pyDes.PAD_PKCS5)
+
+    # crypt or decrypt
+    value = cipher.encrypt(inputText) if isCrypt else cipher.decrypt(inputText)
+
+    # make printable (if needed)
+    if isCrypt:
+        value = "".join( [ "%02.2x" % ord(i) for i in value ] )
+
+    return value
+
+###################################################
+
+def fcn_clear(isCrypt, master, text):
+
+    return text
+
+###################################################
+
+algorithms = {
+    "3DES_CBC":     {
+        "desc"      : "Triple-DES CBC encription (24 bits password + Initialization vector)",
+        "fcn"       : fcn_3DES_CBC
+    },
+    "clear":        {
+        "desc"      : "Clear text password",
+        "fcn"       : fcn_clear
+    }
+}
+
+###################################################
+
+def cmdAlgorithms():
+
+    print ""
+    print "The list of encription algorithms is:"
+    for a in sorted(algorithms.keys()):
+        print "%-10s = %s" % (a, algorithms[a]["desc"])
+    print ""
+
+    return True
+
+###################################################
+
+def cmdCrypt():
+
+    # check we have 3 parameters (plus pname and command)
+    if len(sys.argv) < 5:
+        print ""
+        print "Crypt command requires 3 parameters, use command help"
+        print ""
+        return False
+
+    # get the param values
+    (master, algorithm, text) = (sys.argv[2], sys.argv[3], sys.argv[4])
+
+    # check algorithm is valid
+    if algorithm not in algorithms:
+        print ""
+        print "ERROR: algorithm [%s] is unknown, use command algorithms" % algorithm
+        print ""
+        return False
+
+    # call the algorithm to get the cyphertext
+    cyphertext = algorithms[algorithm]["fcn"](True, master, text)
+
+    # print the encrypted text
+    print """
+The encrypted text is:
+
+%s
+
+""" % cyphertext
+
+    return True
+
+
+###################################################
+
+def cmdDecrypt():
+
+    # check we have 3 parameters (plus pname and command)
+    if len(sys.argv) < 5:
+        print ""
+        print "Decrypt command requires 3 parameters, use command help"
+        print ""
+        return False
+
+    # get the param values
+    (master, algorithm, cyphertext) = (sys.argv[2], sys.argv[3], sys.argv[4])
+
+    # check algorithm is valid
+    if algorithm not in algorithms:
+        print ""
+        print "ERROR: algorithm [%s] is unknown, use command algorithms" % algorithm
+        print ""
+        return False
+
+    # call the algorithm to get the clear
+    text = algorithms[algorithm]["fcn"](False, master, cyphertext)
+
+    # print the encrypted text
+    print """
+The decrypted text is:
+
+%s
+
+""" % text
+
+    return True
+
+###################################################
+
+def cmdHelp():
+    """Print usage help"""
+
+    print """
+usage: python keygen.py <command> [<params>]
+
+where command is one of:
+
+- algorithms --- list the encryption algorithms
+- crypt --- crypt some text. Requires 3 params: master-key algorithm text
+- decrypt --- decrypt some text. Requires 3 params: master-key algorithm text
+- help --- this screen
+- master --- requires no params, prints a master key
+"""
+
+    return True
+
+###################################################
+
+def cmdMaster():
+
+    # generate a 
+    master = "".join( [ "%02.2x" % random.randint(0, 255) for i in range(64) ] )
+
+    # print the master key
+    print ""
+    print "The new master key is\n\n%s" % master
+    print ""
+    print "You must edit keychain.yaml and add the following:"
+    print """
+master-key:
+    data: %s
+    """ % master
+    print ""
+    print ""
+
+    return True
+
+###################################################
+
+commands = {
+    "algorithms":   cmdAlgorithms,
+    "crypt":        cmdCrypt,
+    "decrypt":      cmdDecrypt,
+    "help":         cmdHelp,
+    "master":       cmdMaster
+}
+
+def main():
+
+    # figure out the command (default is help)
+    cmd = sys.argv[1] if len(sys.argv) > 1 else "help"
+    if cmd not in commands:
+        cmd = "help"
+
+    # dispatch the command
+    commands[cmd]()
+
+
+if __name__ == "__main__":
+    main()
